9cea388e7a90a46aaefed4d1cf275904ddb903cc
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-09-28 11:58:48 +0530

    starting to create multiple image functionality

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { HiCamera } from "react-icons/hi";
-import { Button } from "./ui/button";
+
 
 const Modal = () => {
   const modalRef = useRef<HTMLDivElement>(null);
@@ -23,29 +23,46 @@ const Modal = () => {
 
   return (
     <div className="w-full h-full ">
-      <div
-        ref={modalRef}
-        className=""
-      >
+      <div ref={modalRef} className="">
         {selectedFile ? (
-            <div className="flex  flex-col h-full w-full justify-center items-center m-2 p-2 cursor-pointer object-cover">
-              <img className="border border-accent rounded-lg p-2" src={imageUrl} alt="selected file" onClick={() => setSelectedFile(null)} height={100} width={200} />
+          <div className="flex flex-row">
+            <div className=" h-full w-full m-2 p-2 cursor-pointer object-cover">
+              <img
+                className="border border-accent rounded-lg p-2"
+                src={imageUrl}
+                alt="selected file"
+                onClick={() => setSelectedFile(null)}
+                height={100}
+                width={200}
+              />
             </div>
+            <div
+              className="border h-[150px] w-[50%] border-accent hover:text-accent-hover  bg-transparent text-accent hover:border-accent-hover    cursor-pointer rounded-lg flex justify-center items-center "
+              onClick={() => filePickerRef.current?.click()}
+            >
+              <HiCamera className="text-3xl text-accent-hover cursor-pointer" />
+              <input
+                hidden
+                ref={filePickerRef}
+                type="file"
+                accept="image/*"
+                onChange={addImage}
+              />
+            </div>
+          </div>
         ) : (
           <div
             className="border h-[150px] w-full border-accent hover:text-accent-hover  bg-transparent text-accent hover:border-accent-hover    cursor-pointer rounded-lg flex justify-center items-center "
             onClick={() => filePickerRef.current?.click()}
           >
-
-            <HiCamera className="text-3xl text-white/60 cursor-pointer" />
+            <HiCamera className="text-3xl text-accent-hover cursor-pointer" />
             <input
               hidden
               ref={filePickerRef}
               type="file"
               accept="image/*"
               onChange={addImage}
-              />
-
+            />
           </div>
         )}
       </div>
