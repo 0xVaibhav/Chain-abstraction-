2ee6341eb160e53ed36323ba3709962bf3fd9472
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-09-30 15:19:01 +0530

    installed thirdweb and added all the required files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,10 +4,12 @@ import "./globals.css";
 import Header from "@/components/navbar/Header";
 import StairTransition from "@/components/animation/StairTransition";
 import PageTransition from "@/components/animation/PageTransition";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export const metadata: Metadata = {
   title: "Green Threads",
-  description: "A website help reduce waste from manufacturing and increase profits of the manufacturer",
+  description:
+    "A website help reduce waste from manufacturing and increase profits of the manufacturer",
 };
 
 export default function RootLayout({
@@ -18,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-primary text-white">
-        <Header />
-        <StairTransition />
-        <PageTransition>
-          <main>{children}</main>
-        </PageTransition>
+        <ThirdwebProvider>
+          <Header />
+          <StairTransition />
+          <PageTransition>
+            <main>{children}</main>
+          </PageTransition>
+        </ThirdwebProvider>
       </body>
     </html>
   );
